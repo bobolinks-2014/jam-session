f90dae0a18b4b0b77348913d02547bcc69caef8f
Angela Kosek & Grace Yim <pair+angkosek.graceekyim@devbootcamp.com>
2014-08-27 16:35:25 -0500

    moved login and logout methods to sessions controller

--- jam_session/app/controllers/users_controller.rb
+++ jam_session/app/controllers/users_controller.rb
@@ -13,19 +13,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def login
-    @user = User.find_by_email(params[:user][:email])
-      if @user && @user.authenticate(params[:user][:password])
-          session[:user_id] = @user.id
-      end
-      redirect_to root_path
-  end
-
-  def logout
-    session[:user_id] = nil
-    redirect_to root_path
-  end
-
 private
 
   def user_params
