6985c59a389d82080f155016fd011453c6bd86e1
Grace Yim & maddisonb <pair+graceekyim.maddisonb@devbootcamp.com>
2014-08-27 18:19:15 -0500

    added associations to models

--- jam_session/app/models/comment.rb
+++ jam_session/app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ActiveRecord::Base
+belongs_to :commenter, class_name: "User"
+belongs_to :receiver, class_name: "User"
 end
--- jam_session/app/models/user.rb
+++ jam_session/app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+   belongs_to :neighborhood
+   has_many :user_instruments
+   has_many :instruments, through: :user_instruments
+   has_many :tags, through: :user_instruments
+   has_many :comments, class_name: "Comment", foreign_key: "commenter_id"
+   has_many :feedback, class_name: "Comment", foreign_key: "receiver_id"
+
+
+   has_secure_password
 end
