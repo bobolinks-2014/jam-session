ed45f2e6678157715f04bc38dd5a8c2c2bf71656
Grace Yim & maddisonb <pair+graceekyim.maddisonb@devbootcamp.com>
2014-08-28 16:20:37 -0500

    added model shoulda specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../spec_helper.rb'
+
+describe User do
+  it { should belong_to(:neighborhood) }
+  it { should have_many(:user_instruments)}
+  it { should have_many(:instruments).through(:user_instruments) }
+  it { should have_many(:comments).with_foreign_key('commenter_id') }
+  it { should have_many(:feedback).class_name("Comment").with_foreign_key("receiver_id")}
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
+  it { should validate_presence_of(:neighborhood) }
+end
