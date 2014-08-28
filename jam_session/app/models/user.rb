5c2d1269ac266b5c40b9ca4c4aaf0962302a43d4
Grace Yim <graceekim@gmail.com>
2014-08-28 08:02:12 -0500

    updated association

--- jam_session/app/models/user.rb
+++ jam_session/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
    has_many :user_instruments
    has_many :instruments, through: :user_instruments
    has_many :tags, through: :user_instruments
-   has_many :comments, class_name: "Comment", foreign_key: "commenter_id"
+   has_many :comments, foreign_key: "commenter_id"
    has_many :feedback, class_name: "Comment", foreign_key: "receiver_id"
 
    has_secure_password
