c844abbac1c2b829ae8e2b81873794559167e849
Marek Pyczkowski & Ravi <pair+marekpwk.ravip0711@devbootcamp.com>
2014-08-29 10:51:48 -0500

    added instrument model and user model. migrations as well

--- app/models/user_instrument.rb
+++ app/models/user_instrument.rb
@@ -1,8 +1,4 @@
 class UserInstrument < ActiveRecord::Base
   belongs_to :user
   belongs_to :instrument
-
-  acts_as_taggable_on :genres
-
-  validates_presence_of :user_id, :instrument_id, :skill_level
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,70 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827214515) do
+ActiveRecord::Schema.define(version: 20140829154813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comments", force: true do |t|
-    t.text     "content"
-    t.integer  "rating"
-    t.integer  "commenter_id"
-    t.integer  "receiver_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "instruments", force: true do |t|
-    t.string   "name"
-    t.string   "icon"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "neighborhoods", force: true do |t|
-    t.string   "name"
-    t.string   "img_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "taggings", force: true do |t|
-    t.integer  "tag_id"
-    t.integer  "taggable_id"
-    t.string   "taggable_type"
-    t.integer  "tagger_id"
-    t.string   "tagger_type"
-    t.string   "context",       limit: 128
-    t.datetime "created_at"
-  end
-
-  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
-
-  create_table "tags", force: true do |t|
-    t.string  "name"
-    t.integer "taggings_count", default: 0
-  end
-
-  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
-
   create_table "user_instruments", force: true do |t|
     t.integer  "user_id"
     t.integer  "instrument_id"
-    t.integer  "skill_level"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.string   "img_url"
-    t.text     "description"
-    t.integer  "neighborhood_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
