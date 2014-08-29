0f0ee1d85223a7470f5211656fe5feb0ab52d189
Darrell <darrelldjones13@gmail.com>
2014-08-29 14:59:48 -0500

    users controller specs in progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,28 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      @neighborhood = @user.neighborhood
+      redirect_to neighborhood_path(@neighborhood)
     else
       render 'new'
     end
   end
 
+  #edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      @neighborhood = @user.neighborhood
+      redirect_to neighborhood_path(@neighborhood)
+    else
+      render 'edit'
+    end
+  end
 
 private
 
@@ -25,4 +41,3 @@ private
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :tag_list)
   end
 end
-
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
+  let(:user) {User.create!(first_name: "Christopher",
+          last_name: "Walken",
+          email: "needmore@cowbell.org",
+          password: "testing",
+          password_confirmation: "testing",
+          img_url: 'http://i.ytimg.com/vi/ZNT9E3yNaDU/maxresdefault.jpg',
+          description: 'I got a fevah, and the only prescription...is more cowbell',
+          neighborhood: hood)}
+  let(:hood) {Neighborhood.create!(name: "River North")}
 
+  describe "#new" do
+    it "assigns the user to User.new" do
+      get :new
+      expect(assigns(:user)).to be_a_new(User)
+    end
+  end
+
+  describe "#create" do
+    it "creates a new user if params are valid" do
+      expect {
+        post :create, user: {
+          first_name: "Christopher",
+          last_name: "Walken",
+          email: "needmore@cowbell.org",
+          password: "testing",
+          password_confirmation: "testing",
+          img_url: 'http://i.ytimg.com/vi/ZNT9E3yNaDU/maxresdefault.jpg',
+          description: 'I got a fevah, and the only prescription...is more cowbell',
+          neighborhood: hood
+        }
+      }.to change {User.count}.by(1)
+    end
+  end
+
+  describe "#edit" do
+    it "finds the user to be edited" do
+      get :edit, id: user.id
+      expect(assigns(:user)).to eq(user)
+    end
+  end
+
+  describe "#update" do
+    it "updates the user" do
+      expect {
+        put :update, id: user.id, user: {
+          first_name: "Christopher",
+          last_name: "Walken",
+          email: "needmore@cowbell.org",
+          password: "testing",
+          password_confirmation: "testing",
+          img_url: 'http://i.ytimg.com/vi/ZNT9E3yNaDU/maxresdefault.jpg',
+          description: 'I got a fevah, and the only prescription...is more cowbell',
+          neighborhood: hood
+        }
+      }.to change {user.reload.last_name}.to('Walken')
+    end
+  end
+
+  describe "#show" do
+    it 'shows the user' do
+      get :show, id: user.id
+      expect(assigns(:user)).to eq(user)
+    end
+  end
 end
