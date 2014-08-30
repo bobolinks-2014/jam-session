34898878c23313079b9ee8e3a000e36abb6992cf
Grace Yim <graceekim@gmail.com>
2014-08-29 21:41:13 -0500

    updated seed and rake

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,26 +8,28 @@
 
 # Users
 
-User.create(first_name: "Christopher", 
-            last_name: "Walken", 
-            email: "needmore@cowbell.org", 
-            password: "testing", 
-            password_confirmation: "testing", 
-            img_url: 'http://i.ytimg.com/vi/ZNT9E3yNaDU/maxresdefault.jpg', 
-            description: 'I got a fevah, and the only prescription...is more cowbell', 
+User.create(first_name: "Christopher",
+            last_name: "Walken",
+            email: "needmore@cowbell.org",
+            password: "testing",
+            password_confirmation: "testing",
+            img_url: 'http://i.ytimg.com/vi/ZNT9E3yNaDU/maxresdefault.jpg',
+            description: 'I got a fevah, and the only prescription...is more cowbell',
             neighborhood_id: 2)
 
-User.create(first_name: "Axel", last_name: "Rose", email: "sweetchild@omine.org", password: "testing", password_confirmation: "testing", img_url: 'http://iv1.lisimg.com/image/2766251/600full-axl-rose.jpg', description: 'Nanananananannana neeee neeeee', neighborhood_id: 2)
+User.create(first_name: "Axel", last_name: "Rose", email: "sweetchild@omine.org", password: "testing", password_confirmation: "testing", img_url: 'axl_rose.jpeg', description: 'Nanananananannana neeee neeeee', neighborhood_id: 2)
 
-User.create(first_name: "Freddy", last_name: "Mercury", email: "moustache@queen.org", password: "testing", password_confirmation: "testing", img_url: 'http://31.media.tumblr.com/tumblr_mbqu2agCWo1qdslbvo1_1280.png', description: 'People on the streets. Ba-da-dee-dop-dop', neighborhood_id: 1)
+User.create(first_name: "Freddy", last_name: "Mercury", email: "moustache@queen.org", password: "testing", password_confirmation: "testing", img_url: 'mercury.png', description: 'People on the streets. Ba-da-dee-dop-dop', neighborhood_id: 1)
 
-User.create(first_name: "Rivers", last_name: "Cuomo", email: "rivers@weezer.org", password: "testing", password_confirmation: "testing", img_url: 'http://blogs.houstonpress.com/rocks/riverscuomo-718718.jpg', description: 'Beverly Hills, thats where I want to be', neighborhood_id: 2)
+User.create(first_name: "Rivers", last_name: "Cuomo", email: "rivers@weezer.org", password: "testing", password_confirmation: "testing", img_url: 'cuomo.jpg', description: 'Beverly Hills, thats where I want to be', neighborhood_id: 2)
 
-User.create(first_name: "Henry", last_name: "Rollins", email: "henry@blackflag.org", password: "testing", password_confirmation: "testing", img_url: 'http://www.nndb.com/people/527/000022461/HenryR1.jpg', description: 'Whats the matter man? Is the time not right?', neighborhood_id: 3)
+User.create(first_name: "Henry", last_name: "Rollins", email: "henry@blackflag.org", password: "testing", password_confirmation: "testing", img_url: 'rollins.jpg', description: 'Whats the matter man? Is the time not right?', neighborhood_id: 3)
 
-User.create(first_name: "Kenny", last_name: "G", email: "thebest@clarinet.org", password: "testing", password_confirmation: "testing", img_url: 'http://www.littlekidsrock.org/wp-content/uploads/2013/11/kenny-g-curls.jpg', description: 'Oh my gosh Kenny, that song sucks.', neighborhood_id: 4)
+User.create(first_name: "Kenny", last_name: "G", email: "thebest@clarinet.org", password: "testing", password_confirmation: "testing", img_url: 'kenny_g.jpg', description: 'Oh my gosh Kenny, that song sucks.', neighborhood_id: 4)
 
-User.create(first_name: "Tina", last_name: "Turner", email: "simplytheest@tina.org", password: "testing", password_confirmation: "testing", img_url: 'http://ia.media-imdb.com/images/M/MV5BMTYxNzEwNzQxOF5BMl5BanBnXkFtZTYwMjUwMjg2._V1_SY317_CR32,0,214,317_AL_.jpg', description: 'Whats love got to do, got to do with it?', neighborhood_id: 1)
+User.create(first_name: "Tina", last_name: "Turner", email: "simplytheest@tina.org", password: "testing", password_confirmation: "testing", img_url: 'turner.jpg', description: 'Whats love got to do, got to do with it?', neighborhood_id: 1)
+
+User.create(first_name: "Joan", last_name: "Jett", email: "joan@runaways.org", password: "testing", password_confirmation: "testing", img_url: 'jett.jpg', neighborhood_id: 1)
 
 
 # Instruments
