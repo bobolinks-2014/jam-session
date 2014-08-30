e12f16f6d7d85e30fafdfde5b68be18513f76122
Grace Yim <graceekim@gmail.com>
2014-08-30 13:27:28 -0500

    updated seeds file, needs a little more work

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,38 +8,16 @@
 
 # Users
 
-User.create(first_name: "Christopher",
-            last_name: "Walken",
-            email: "needmore@cowbell.org",
-            password: "testing",
-            password_confirmation: "testing",
-            img_url: 'http://i.ytimg.com/vi/ZNT9E3yNaDU/maxresdefault.jpg',
-            description: 'I got a fevah, and the only prescription...is more cowbell',
-            neighborhood_id: 2)
-
-User.create(first_name: "Axel", last_name: "Rose", email: "sweetchild@omine.org", password: "testing", password_confirmation: "testing", img_url: 'axl_rose.jpeg', description: 'Nanananananannana neeee neeeee', neighborhood_id: 2)
-
-User.create(first_name: "Freddy", last_name: "Mercury", email: "moustache@queen.org", password: "testing", password_confirmation: "testing", img_url: 'mercury.png', description: 'People on the streets. Ba-da-dee-dop-dop', neighborhood_id: 1)
-
-User.create(first_name: "Rivers", last_name: "Cuomo", email: "rivers@weezer.org", password: "testing", password_confirmation: "testing", img_url: 'cuomo.jpg', description: 'Beverly Hills, thats where I want to be', neighborhood_id: 2)
-
-User.create(first_name: "Henry", last_name: "Rollins", email: "henry@blackflag.org", password: "testing", password_confirmation: "testing", img_url: 'rollins.jpg', description: 'Whats the matter man? Is the time not right?', neighborhood_id: 3)
-
-User.create(first_name: "Kenny", last_name: "G", email: "thebest@clarinet.org", password: "testing", password_confirmation: "testing", img_url: 'kenny_g.jpg', description: 'Oh my gosh Kenny, that song sucks.', neighborhood_id: 4)
-
-User.create(first_name: "Tina", last_name: "Turner", email: "simplytheest@tina.org", password: "testing", password_confirmation: "testing", img_url: 'turner.jpg', description: 'Whats love got to do, got to do with it?', neighborhood_id: 1)
-
-User.create(first_name: "Joan", last_name: "Jett", email: "joan@runaways.org", password: "testing", password_confirmation: "testing", img_url: 'jett.jpg', neighborhood_id: 1)
 
 
 # Instruments
 
-Instrument.create(name: "Vocals")
-Instrument.create(name: "Guitar")
-Instrument.create(name: "Saxophone")
-Instrument.create(name: "Drums")
-Instrument.create(name: "Cowbell")
-Instrument.create(name: "Clarinet")
+Instrument.create(name: "Vocals", icon: 'microphone-icon.png')
+Instrument.create(name: "Guitar", icon: 'guitar-icon.png')
+Instrument.create(name: "Saxophone", icon: 'sax-icon.png')
+Instrument.create(name: "Drums", icon: 'drum-icon.png')
+Instrument.create(name: "Cowbell", icon: 'cowbell-icon.png')
+# Instrument.create(name: "Clarinet")
 
 
 # Neighborhoods
@@ -51,6 +29,28 @@ Neighborhood.create(name: 'Lakeview')
 Neighborhood.create(name: 'Hyde Park')
 Neighborhood.create(name: 'River North')
 
+User.create(first_name: "Christopher",
+            last_name: "Walken",
+            email: "needmore@cowbell.org",
+            password: "testing",
+            password_confirmation: "testing",
+            img_url: 'walken.jpg',
+            description: 'I got a fevah, and the only prescription...is more cowbell',
+            neighborhood_id: 2)
+
+User.create(first_name: "Axel", last_name: "Rose", email: "sweetchild@omine.org", password: "testing", password_confirmation: "testing", img_url: 'axl_rose.jpeg', description: 'Nanananananannana neeee neeeee', neighborhood_id: 2)
+
+User.create(first_name: "Freddy", last_name: "Mercury", email: "moustache@queen.org", password: "testing", password_confirmation: "testing", img_url: 'mercury.png', description: 'People on the streets. Ba-da-dee-dop-dop', neighborhood_id: 1)
+
+User.create(first_name: "Rivers", last_name: "Cuomo", email: "rivers@weezer.org", password: "testing", password_confirmation: "testing", img_url: 'cuomo.jpg', description: 'Beverly Hills, thats where I want to be', neighborhood_id: 2)
+
+User.create(first_name: "Henry", last_name: "Rollins", email: "henry@blackflag.org", password: "testing", password_confirmation: "testing", img_url: 'rollins.jpg', description: 'Whats the matter man? Is the time not right?', neighborhood_id: 3)
+
+User.create(first_name: "Kenny", last_name: "G", email: "thebest@clarinet.org", password: "testing", password_confirmation: "testing", img_url: 'kenny_g_2.jpg', description: 'Awesome musician looking for other awesome musicians.', neighborhood_id: 4)
+
+User.create(first_name: "Tina", last_name: "Turner", email: "simplytheest@tina.org", password: "testing", password_confirmation: "testing", img_url: 'turner.jpg', description: 'Whats love got to do, got to do with it?', neighborhood_id: 1)
+
+User.create(first_name: "Joan", last_name: "Jett", email: "joan@runaways.org", password: "testing", password_confirmation: "testing", img_url: 'jett.jpg', neighborhood_id: 1)
 # Comments
 
 Comment.create(content: 'This guy was so great to play with.  Musical Genius', rating: rand(1..5), commenter_id: rand(1..7), receiver_id: rand(1..7))
@@ -70,7 +70,7 @@ Comment.create(content: 'Legendary skills...psyche!', rating: rand(1..5), commen
 # user_instruments
 
 10.times do
-  UserInstrument.create(user_id: rand(1..7), instrument_id: rand(1..7), skill_level: rand(1..5))
+  UserInstrument.create(user_id: rand(1..7), instrument_id: rand(1..5), skill_level: rand(1..5))
 end
 
 # tag-list
