d93c0bf50cc908d5c62e614a44eafdf5359ddad0
Grace Yim <graceekim@gmail.com>
2014-08-30 22:03:40 -0500

    added jam sesh model and migration

---
+++ app/models/jam_sesh.rb
@@ -0,0 +1,15 @@
+class JamSesh < ActiveRecord::Base
+  has_many :user_jam_seshes
+  validates_presence_of :sesh_date
+
+  def format_date
+    date = self.sesh_date
+    date.month.to_s + "." + date.day.to_s + "." + date.year.to_s
+  end
+
+  def get_users
+    seshes = UserJamSesh.where(jam:self)
+    seshes.map {|x| x.user }
+  end
+
+end
