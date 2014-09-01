9831143b42b478014e6220b55c02fb9bcc112531
Grace Yim <graceekim@gmail.com>
2014-09-01 08:16:40 -0500

    fixed nav bar

--- spec/model/comment_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Comment, :type => :model do
-  it { should belong_to(:commenter).class_name("User") }
-  it { should belong_to(:receiver).class_name("User") }
-end
