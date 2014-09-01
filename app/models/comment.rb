68d91e64d9e54adfa726e01ab796ed318cc71a17
Grace Yim <graceekim@gmail.com>
2014-08-31 21:17:20 -0500

    cleaned up old models

--- app/models/comment.rb
+++
@@ -1,4 +0,0 @@
-class Comment < ActiveRecord::Base
-belongs_to :commenter, class_name: "User"
-belongs_to :receiver, class_name: "User"
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,6 @@ class User < ActiveRecord::Base
     belongs_to :neighborhood
     has_many :user_instruments
     has_many :instruments, through: :user_instruments
-    has_many :comments, foreign_key: "commenter_id"
-    has_many :feedback, class_name: "Comment", foreign_key: "receiver_id"
-    has_many :received_messages, class_name: "Message", foreign_key: "receiver_id"
-    has_many :sent_messages, class_name: "Message", foreign_key: "sender_id"
     has_many :user_jam_seshes
     has_many :jam_seshes, through: :user_jam_seshes
     has_many :created_jam_seshes, class_name: "JamSesh", foreign_key: "creator_id"
